16fd96f651086913235dbe3f8ddf8f9de4401509
Mehedi ScriptDev <mehedi25122003@gmail.com>
2025-12-24 00:04:34 +0600

    necessaries installed

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port,(()=>{
+    console.log(`Server is running on port ${port}`);
+}))
+
