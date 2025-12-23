883476aec85794f8839725cf15887ce33a31c598
Mehedi ScriptDev <mehedi25122003@gmail.com>
2025-12-24 02:35:23 +0600

    post and get added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,12 +10,62 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wld9ndi.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let newitemsCollections;
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    newitemsCollections = client.db('reeniDB').collection('newitems');
+    console.log('Connected to MongoDB!');
+  } catch (err) {
+    console.error('MongoDB connection error:', err.message);
+  }
+}
+run().catch(console.dir);
+
+// post api
+app.post('/new-list', async (req,res)=>{
+    try {
+      const newLIst = req.body;
+      const results = await newitemsCollections.insertOne(newLIst)
+      res.send(results)
+    } catch (err) {
+      res.status(500).send({ error: err.message })
+    }
+})
+
+// get api - fetch all items
+app.get('/new-list', async (req,res)=>{
+    try {
+      const allItems = await newitemsCollections.find({}).toArray();
+      res.send(allItems);
+    } catch (err) {
+      res.status(500).send({ error: err.message })
+    }
+})
 
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
 
 app.listen(port,(()=>{
     console.log(`Server is running on port ${port}`);
